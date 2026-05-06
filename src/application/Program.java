362f716c889fe7155e1af14cc76175e9edd7e680
vsbreno <brenovinicius.santos0@gmail.com>
2026-05-06 11:17:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
 		
-		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
